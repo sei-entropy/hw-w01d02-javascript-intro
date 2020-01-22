4645e1629521236c2cea72b941fb0c926d2a656c
RanenDev <github/RanenDev>
2020-01-22 21:10:07 +0300

    solution homework2

---
+++ homework2.js
@@ -0,0 +1,17 @@
+console.log('Please ' + 'squeeze ' + 'the ' + 'cheese');
+
+const word = 'believe';
+
+console.log('Please ' + word + ' the ' + 'cheese');
+
+let num1 = 5;
+let num2 = 10;
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + (num1 +num2));
+
+
+
+//for loops
+
+for (i = 1; i <= 1000; i++) {
+    console.log('The number is: ' + i);
+}
