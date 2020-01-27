0ce5636afd53d74de65a4743e298d002f297963c
RanenDev <github/RanenDev>
2020-01-27 09:52:48 +0300

    solve hw

--- homework2.js
+++ homework2.js
@@ -1,17 +1,97 @@
-console.log('Please ' + 'squeeze ' + 'the ' + 'cheese');
 
-const word = 'believe';
+let a = 1;
+let b = 'bongos';
+let c = true;
+a = b;
+b = c;
+c = a;
 
-console.log('Please ' + word + ' the ' + 'cheese');
+/*What is a?
+a is 'bongos'
+What is b?
+b is true
+What is c?
+c is 'bongos'
+*/
 
-let num1 = 5;
-let num2 = 10;
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + (num1 +num2));
+const firstWord = "Please";
+const secondWord = "squeeze";
+const thirdWord = "the";
+const fourthWord = "cheese";
+console.log(firstWord+" "+secondWord+" "+thirdWord+" "+fourthWord);
+
+//Result ==> "Please squeeze the cheese"
 
 
+const num1 = 5;
+const num2 = 10;
+const num3 = num1 + num2
+console.log("The sum of " + num1 + " and " + num2 + " is " + num3);
+//Result ==> The sum of 5 and 10 is 15
 
-//for loops
+//7
+999 > 999
+999 === 999 
+999 !== 999
+-5 >= -4
+100 <= -100
+20 + 5 < 5 
+81 / 9 === 9
+9 !== 8 + 1
+/*Result ==> 
+a) false
+b) true
+c) false
+d) false
+e) false
+f) false
+g) true
+h) false
+*/
 
-for (i = 1; i <= 1000; i++) {
-    console.log('The number is: ' + i);
+
+let num = 30
+if (num%3 == 0 && num%5 == 0){ 
+console.log("FizzBuzz")
+}else if (num%3 == 0){
+console.log("Fizz")
+}else if (num%5 == 0){
+console.log("Buzz")
+}else{
+console.log(num)
 }
+//Result ==> FizzBuzz
+
+
+let num = 0
+while(num <= 100){
+    if (num%3 == 0 && num%5 == 0){
+    console.log("FizzBuzz")
+}else if (num%3 == 0){
+    console.log("Fizz")
+}else if (num%5 == 0){ 
+    console.log("Buzz")
+}else{ 
+console.log(num)
+}
+    num++;
+}
+//Result ==> done
+
+
+//10
+function maxNum(max){
+    let num = 0   
+    while(num <= max){
+       if (num%3 == 0 && num%5 == 0){
+       console.log("FizzBuzz")
+       }else if (num%3 == 0){ 
+       console.log("Fizz")
+       }else if (num%5 == 0){ 
+       console.log("Buzz")
+       }else{
+       console.log(num)
+       }
+       num++;
+   }   
+   }
